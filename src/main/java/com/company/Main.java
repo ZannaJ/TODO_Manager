ecb0725246d6ac0680677640721025f26a3fafde
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-08-23 19:49:05 +0300

    add login

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
  //       System.out.println(Login.userSignUp() ? "Added a new user." : "Can't add a new user.");
 //        System.out.println(ManagerController.addNewTask() ? "Successfully added new task" : "Failed to add a new task. ");
-        System.out.println(Login.login() ? "Added a new user." : "Can't add a new user.");
+        System.out.println(Login.login() ? "Sucessfully logged in" : "Failed to login in");
 
 
     }
